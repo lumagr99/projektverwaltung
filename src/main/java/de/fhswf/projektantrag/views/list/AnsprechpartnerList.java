928723f88c6dda9c9eae6fb60e09eab8c9016037
lumagr99 <lumagr.mail@gmail.com>
2021-01-05 16:23:14 +0100

    Auf die BenutzerListe angepasst.

--- src/main/java/de/fhswf/projektantrag/views/list/AnsprechpartnerList.java
+++ src/main/java/de/fhswf/projektantrag/views/list/AnsprechpartnerList.java
@@ -1,7 +1,5 @@
 package de.fhswf.projektantrag.views.list;
 
-import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import de.fhswf.projektantrag.data.entities.BenutzerEntity;
@@ -9,45 +7,21 @@ import de.fhswf.projektantrag.manager.OrganisationManager;
 import de.fhswf.projektantrag.views.main.MainView;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.annotation.PostConstruct;
+import java.util.List;
 
 /**
  * Erstellt eine ggf. Organisationsspezifische Ansicht von Ansprechpartnern.
  */
 @Route(value = "ansprechpartner", layout = MainView.class)
 @PageTitle("Ansprechpartner | ProjektAntrag")
-public class AnsprechpartnerList extends VerticalLayout {
+public class AnsprechpartnerList extends BenutzerList {
 
     @Autowired
     OrganisationManager organisationManager;
 
-    private Grid<BenutzerEntity> grid;
 
-    AnsprechpartnerList(OrganisationManager organisationManager){
-        setId("project-list-view");
-        addClassName("project-list-view");
-        setSizeFull();
-    }
-
-    @PostConstruct
-    private void init(){
-
-        grid = new Grid<BenutzerEntity>(BenutzerEntity.class);
-        configureGrid();
-
-        add(grid);
-    }
-
-    private void configureGrid(){
-        grid.addClassName("ansprechpartner-grid");
-        grid.setSizeFull();
-        grid.getColumns().forEach(col -> col.setAutoWidth(true));
-        grid.setColumns("vorname", "nachname");
-        updateList();
-    }
-
-    private void updateList() {
-        grid.setItems(organisationManager.getAnsprechpartner());
+    List<BenutzerEntity> updateList() {
+        return organisationManager.getAnsprechpartner();
     }
 
 
