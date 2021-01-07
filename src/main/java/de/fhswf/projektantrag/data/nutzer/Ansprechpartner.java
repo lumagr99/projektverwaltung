8cd637bd2b0aa109e74869ba63976b4751f466dd
lumagr99 <lumagr.mail@gmail.com>
2021-01-07 11:36:42 +0100

    Grundlegende Funktionen von Student und Ansprechpartner erstellt.

--- src/main/java/de/fhswf/projektantrag/data/nutzer/Ansprechpartner.java
+++ src/main/java/de/fhswf/projektantrag/data/nutzer/Ansprechpartner.java
@@ -1,4 +1,21 @@
 package de.fhswf.projektantrag.data.nutzer;
 
-public class Ansprechpartner {
+import de.fhswf.projektantrag.data.entities.BenutzerEntity;
+
+public class Ansprechpartner extends BenutzerEntity {
+
+    public Ansprechpartner(BenutzerEntity benutzerEntity){
+        if(benutzerEntity == null || benutzerEntity.getRollenEntity().getId() != 3){
+            throw new IllegalArgumentException("Dieser Benutzer ist kein Ansprechpartner");
+        }
+
+        this.setBenutzername(benutzerEntity.getBenutzername());
+        this.setVorname(benutzerEntity.getVorname());
+        this.setNachname(benutzerEntity.getNachname());
+        this.setPasswort(benutzerEntity.getPasswort());
+        this.setId(benutzerEntity.getId());
+        this.setOrganisationId(benutzerEntity.getOrganisationId());
+        this.setRollenEntity(benutzerEntity.getRollenEntity());
+    }
+
 }
--- src/main/java/de/fhswf/projektantrag/data/nutzer/Student.java
+++ src/main/java/de/fhswf/projektantrag/data/nutzer/Student.java
@@ -1,4 +1,21 @@
 package de.fhswf.projektantrag.data.nutzer;
 
-public class Student {
+import de.fhswf.projektantrag.data.entities.BenutzerEntity;
+
+public class Student extends BenutzerEntity {
+
+    public Student(BenutzerEntity benutzerEntity){
+        if(benutzerEntity == null || benutzerEntity.getRollenEntity().getId() != 1){
+            throw new IllegalArgumentException("Dieser Benutzer ist kein Student");
+        }
+
+        this.setBenutzername(benutzerEntity.getBenutzername());
+        this.setVorname(benutzerEntity.getVorname());
+        this.setNachname(benutzerEntity.getNachname());
+        this.setPasswort(benutzerEntity.getPasswort());
+        this.setId(benutzerEntity.getId());
+        this.setOrganisationId(benutzerEntity.getOrganisationId());
+        this.setRollenEntity(benutzerEntity.getRollenEntity());
+    }
+
 }
